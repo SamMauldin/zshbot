71388fe3c4a80b383fec23f04fd408a67d513e92
Sam Mauldin <sam@fluidnode.com>
2015-01-10 12:20:45 -0600

    Add framework for chat logging

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ require("./lib/chat");
 require("./lib/bot");
 require("./lib/webserver");
 
+if (config.log.enable) {
+    require("./lib/log");
+}
+
 config.features.forEach(function(v) {
     require("./lib/handlers/" + v);
 });
